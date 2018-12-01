2f6e4bf24a96829bec61c9d70b44c0e789f81bf5
DaniloPaixaoJesus <contato.danilo.paixao@gmail.com>
2018-12-01 15:03:03 -0200

    resolvido problema com build no android

--- android/app/src/main/java/com/instafakereactnative/MainApplication.java
+++ android/app/src/main/java/com/instafakereactnative/MainApplication.java
@@ -2,17 +2,19 @@ package com.instafakereactnative;
 
 import android.app.Application;
 
-import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.reactnativenavigation.NavigationApplication;
+import javax.annotation.Nullable;
+
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
+//public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -41,5 +43,16 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+  }
+
+  @Override
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+
+  @Nullable
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return null;
   }
 }
